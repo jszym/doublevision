5c0144262715290bf2c10a830a875348309cea97
Joseph Szymborski <jszym@localhost.localdomain>
2016-01-23 14:23:57 -0500

    fixed append bug (dyslexia?)

--- app/extract_frames.py
+++ app/extract_frames.py
@@ -33,7 +33,7 @@ def extract(vid_path, f_interval):
     	rval, frame = cv.read()
     	if frame_num % f_interval == 0:
             file = 'frames/{}-{}.jpg'.format(file_name, frame_num)
-            file.append(file_name_array)
+            file_name_array.append(file)
             print file
             cv2.imwrite(file, frame)
             cv2.waitKey(1)
